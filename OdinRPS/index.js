4da614a58964d9da77503f9216a743d3fff75bbc
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-01-31 16:27:13 +0100

    add GUI to Rock Paper Scissors game

--- OdinRPS/index.js
+++ OdinRPS/index.js
@@ -1,5 +1,18 @@
 // Rock paper scissors
 const OPTIONS = ["rock", "paper", "scissors"];
+
+const resultEl = document.querySelector(".result");
+
+document
+  .querySelector(".container.buttons")
+  .addEventListener("click", handleClick);
+document.querySelector("#reset").addEventListener("click", resetGame);
+const buttons = [...document.querySelector(".container.buttons").children];
+
+let computerScore = 0;
+let humanScore = 0;
+updateScore();
+
 function getComputerChoice() {
   return OPTIONS[Math.floor(Math.random() * 3)];
 }
@@ -9,16 +22,23 @@ function getHumanChoice() {
   return OPTIONS.includes(input) ? input : getHumanChoice();
 }
 
-let computerScore = 0;
-let humanScore = 0;
-
 function playRound(computerChoice, humanChoice) {
   const handleWin = (message, winner) => {
-    console.log(message);
+    resultEl.textContent = message;
     if (winner === "human") {
       humanScore++;
     } else {
       computerScore++;
+    }
+    updateScore();
+    if (humanScore === 5 || computerScore === 5) {
+      if (humanScore === 5) {
+        resultEl.textContent = "Game over! Human wins!";
+      } else if (computerScore === 5) {
+        resultEl.textContent = "Game over! Computer wins!";
+      }
+      humanScore = 0;
+      computerScore = 0;
     }
   };
   const combinations = {
@@ -29,7 +49,7 @@ function playRound(computerChoice, humanChoice) {
       handleWin("Computer wins! Rock beats scissors", "computer");
     },
     scissorspaper: () => {
-      handleWin("Computer wins! Scissors beats paper", "human");
+      handleWin("Computer wins! Scissors beats paper", "computer");
     },
     paperrock: () => {
       handleWin("Computer wins! Paper beats rock", "computer");
@@ -38,30 +58,55 @@ function playRound(computerChoice, humanChoice) {
       handleWin("Human wins! Rock beats scissors", "human");
     },
     paperscissors: () => {
-      handleWin("Computer wins! Scissors beats paper", "computer");
+      handleWin("Human wins! Scissors beats paper", "human");
     },
   };
 
   if (computerChoice === humanChoice) {
-    console.log("It's a tie!");
+    resultEl.textContent = "It's a tie!";
   } else {
-    combinations[computerChoice + humanChoice.toLowerCase()]();
+    combinations[computerChoice + humanChoice]();
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+function handleClick(ev) {
+  document
+    .querySelector(".container.buttons")
+    .removeEventListener("click", handleClick);
 
-    playRound(humanSelection, computerSelection);
-  }
-  if (humanScore === computerScore) {
-    console.log("It's a tie! Nobody wins");
-  } else if (humanScore > computerScore) {
-    console.log("Human wins!");
-  } else {
-    console.log("Computer wins!");
+  const humanChoice = ev.target.id;
+  const computerChoice = getComputerChoice();
+  highlightButtons(computerChoice, humanChoice);
+  setTimeout(() => {
+    playRound(computerChoice, humanChoice);
+
+    document
+      .querySelector(".container.buttons")
+      .addEventListener("click", handleClick);
+  }, 300);
+}
+function updateScore() {
+  document.querySelector(".score#human").textContent = `${humanScore}`;
+  document.querySelector(".score#computer").textContent = `${computerScore}`;
+}
+
+function resetGame() {
+  humanScore = computerScore = 0;
+  updateScore();
+  highlightButtons();
+  resultEl.textContent = "Click any button to start game";
+}
+function highlightButtons(computerChoice, humanChoice) {
+  for (const button of buttons) {
+    button.classList.remove("human-choice", "computer-choice", "tie");
+    if ([computerChoice, humanChoice].includes(button.id)) {
+      if (computerChoice === humanChoice) {
+        button.classList.add("tie");
+      } else if (computerChoice === button.id) {
+        button.classList.add("computer-choice");
+      } else {
+        button.classList.add("human-choice");
+      }
+    }
   }
 }
-playGame();
