be41385e87bfe4e17db23d574793746502a17e39
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-07-06 16:23:54 +0200

    feat: make header sticky and change background-color on scroll

--- OdinRestaurantPage/src/index.js
+++ OdinRestaurantPage/src/index.js
@@ -2,5 +2,24 @@ import "./styles.css";
 
 import homepage from "./homepage";
 
-homepage();
+const header = document.querySelector('header');
+const sentinel = document.querySelector('#header-sentinel');
+
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    if (entry.isIntersecting) {
+      header.classList.remove('scrolled');
+    } else {
+      header.classList.add('scrolled');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px 0px 0px 0px'
+  }
+);
+
+observer.observe(sentinel);
+// homepage();
 
